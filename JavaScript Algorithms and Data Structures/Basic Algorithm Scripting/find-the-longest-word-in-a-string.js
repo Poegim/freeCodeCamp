ab0a975bb93385a3fc4906de4dde69bc94bf4525
poegim <jacekwiniarczyk@gmail.com>
2023-04-03 12:00:29 +0200

    Basic Algorith Scripting

---
+++ JavaScript Algorithms and Data Structures/Basic Algorithm Scripting/find-the-longest-word-in-a-string.js
@@ -0,0 +1,17 @@
+function findLongestWordLength(str) {
+
+  const words = str.split(" ");
+  let longest = 0;
+
+  for(let i=0; i < words.length; i++)
+  {
+    if (words[i].length > longest)
+    {
+      longest = words[i].length;
+    }
+  }
+  
+  return longest;
+}
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
