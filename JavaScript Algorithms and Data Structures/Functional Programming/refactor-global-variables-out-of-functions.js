fba7dcd5ad200ea4306ecfc2c921404f20b9d34d
poegim <jacekwiniarczyk@gmail.com>
2023-04-10 19:01:38 +0200

    Functional Programming

---
+++ JavaScript Algorithms and Data Structures/Functional Programming/refactor-global-variables-out-of-functions.js
@@ -0,0 +1,27 @@
+// The global variable
+const bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+// Change code below this line
+function add(bookList, bookName) {
+
+  let localBookList = [...bookList]
+  localBookList.push(bookName);
+  return localBookList;
+  
+  // Change code above this line
+}
+
+// Change code below this line
+function remove(bookList, bookName) {
+  
+  let localBookList = [...bookList]
+  const book_index = localBookList.indexOf(bookName);
+
+  if (book_index >= 0) {
+
+    localBookList.splice(book_index, 1);
+    return localBookList;
+
+    // Change code above this line
+    }
+}
