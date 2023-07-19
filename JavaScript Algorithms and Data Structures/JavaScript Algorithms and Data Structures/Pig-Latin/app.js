9bb7003bb89f17020ded3f604ff628adc80d90c6
Poegim <jacekwiniarczyk@gmail.com>
2023-07-19 17:24:12 +0200

    Pig Latin

---
+++ JavaScript Algorithms and Data Structures/JavaScript Algorithms and Data Structures/Pig-Latin/app.js
@@ -0,0 +1,24 @@
+function translatePigLatin(str) {
+    return str;
+}
+
+//should return the string aliforniacay.
+translatePigLatin("california");
+
+//should return the string aragraphspay.
+translatePigLatin("paragraphs");
+
+//should return the string oveglay.
+translatePigLatin("glove");
+
+//should return the string algorithmway.
+translatePigLatin("algorithm");
+
+//should return the string eightway.
+translatePigLatin("eight");
+
+//Should handle words where the first vowel comes in the middle of the word. Should return the string artzschway.
+translatePigLatin("schwartz");
+
+//Should handle words without vowels. Should return the string rhythmay
+translatePigLatin("rhythm");
