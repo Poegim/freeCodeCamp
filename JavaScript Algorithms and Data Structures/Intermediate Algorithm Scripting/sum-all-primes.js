1e061a584971c54a5e23e196e1901a6b9fd3e384
poegim <jacekwiniarczyk@gmail.com>
2023-07-20 20:20:19 +0200

    Sum primes

---
+++ JavaScript Algorithms and Data Structures/Intermediate Algorithm Scripting/sum-all-primes.js
@@ -0,0 +1,13 @@
+function sumPrimes(num) {
+    return num;
+}
+
+function isInt(n) {
+    return n % 1 === 0;
+ }
+  
+// should return 17.
+console.log(sumPrimes(10));
+
+// should return 73156.
+console.log(sumPrimes(977));
