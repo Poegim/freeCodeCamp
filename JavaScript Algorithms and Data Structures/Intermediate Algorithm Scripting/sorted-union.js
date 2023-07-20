7e0759c138199c24ac5f1de2f5baea824ad4b3c1
poegim <jacekwiniarczyk@gmail.com>
2023-07-20 13:04:56 +0200

    sorted union, intermediate algorithms

---
+++ JavaScript Algorithms and Data Structures/Intermediate Algorithm Scripting/sorted-union.js
@@ -0,0 +1,25 @@
+function uniteUnique(...arr) {
+    let mergedArray = [];
+
+    arr.forEach(element => {
+        element.forEach(el => {
+            if (!mergedArray.includes(el)) {
+                mergedArray.push(el);
+            }
+        })
+    });
+
+    return mergedArray;
+
+}
+
+// should return [1, 3, 2, 5, 4].
+console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
+//  should return [1, 2, 3, 5].
+console.log(uniteUnique([1, 2, 3], [5, 2, 1]));
+//  should return [1, 2, 3, 5, 4, 6, 7, 8].
+console.log(uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]));
+//  should return [1, 3, 2, 5, 4, 6].
+console.log(uniteUnique([1, 3, 2], [5, 4], [5, 6]));
+//  should return [1, 3, 2, 5, 4].
+console.log(uniteUnique([1, 3, 2, 3], [5, 2, 1, 4], [2, 1]));
