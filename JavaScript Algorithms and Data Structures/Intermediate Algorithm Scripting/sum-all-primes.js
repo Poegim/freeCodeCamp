aa2b6c178150c0af25122820decf36621b895ec9
Poegim <jacekwiniarczyk@gmail.com>
2023-07-22 17:16:42 +0200

    Sum primes, intermediate algorithms

--- JavaScript Algorithms and Data Structures/Intermediate Algorithm Scripting/sum-all-primes.js
+++ JavaScript Algorithms and Data Structures/Intermediate Algorithm Scripting/sum-all-primes.js
@@ -1,11 +1,35 @@
 function sumPrimes(num) {
-    return num;
+
+    let sum = 0;
+
+    for (let i = 0; i <= num; i++) {
+        if (isPrime(i)) {
+            sum += i;
+        }
+    }
+
+    return sum;
 }
 
-function isInt(n) {
-    return n % 1 === 0;
- }
-  
+function isPrime(n) {
+    if (n == 0) {
+        return false;
+    }
+
+    if (n == 1) {
+        return false;
+    }
+
+    for (let i = 2; i < n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+
+}
+
 // should return 17.
 console.log(sumPrimes(10));
 
